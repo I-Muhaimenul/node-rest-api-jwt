569be5640dad42ccacb28b287f8ad70b3c8fd8fc
Muhaimenul Islam <muhaimenulislam@iut-dhaka.edu>
2018-08-24 16:09:39 +0600

    changed mongodb to mlab

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # node-rest-api-jwt
-A REST-API developed in NodeJS, ExpressJS, MongoDB, JWT, Multer etc.
+A REST-API for storing products and orders information. Order to Product 12oM relationship. Authentication is needed for order placement. The application is developed in NodeJS, ExpressJS, MongoDB, JWT, Multer etc.
+- Mlab and Mongoose used for MongoDB
+- JesonWebToken package used for api authentication
+- Multer is used fol file (image) upload
--- app.js
+++ app.js
@@ -9,17 +9,22 @@ const orderRoutes = require('./api/routes/orders')
 const userRoutes = require('./api/routes/user')
 
 //mongoose connect with mongodb atlas. it can also be done local mongodb easily
-mongoose.connect( 'mongodb+srv://muhaimenul:'+ process.env.MONGO_ATLAS_PW +'@node-rest-dsafx.mongodb.net/test?retryWrites=true',
-    // 'mongodb://muhaimenul:'+ encodeURIComponent(process.env.MONGO_ATLAS_PW) +'@node-rest-shard-00-00-dsafx.mongodb.net:27017,node-rest-shard-00-01-dsafx.mongodb.net:27017,node-rest-shard-00-02-dsafx.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shard-0&authSource=admin&retryWrites=true',
-    {
-    //    useMongoClient: true
-        useNewUrlParser: true
-    },
-    () => {
-        console.log('Mongodb connected')
-    }
-).catch(err => {console.log(err)})
+// mongoose.connect( 'mongodb+srv://muhaimenul:'+ process.env.MONGO_ATLAS_PW +'@node-rest-dsafx.mongodb.net/test?retryWrites=true',
+//     // 'mongodb://muhaimenul:'+ encodeURIComponent(process.env.MONGO_ATLAS_PW) +'@node-rest-shard-00-00-dsafx.mongodb.net:27017,node-rest-shard-00-01-dsafx.mongodb.net:27017,node-rest-shard-00-02-dsafx.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shard-0&authSource=admin&retryWrites=true',
+//     {
+//     //    useMongoClient: true
+//         useNewUrlParser: true
+//     },
+//     () => {
+//         console.log('Mongodb connected')
+//     }
+// ).catch(err => {console.log(err)})
+// for connecting mlab
+const mongoUri = 'mongodb://muhaimenul:873965ove@ds119652.mlab.com:19652/practice'
 mongoose.Promise = global.Promise //for deprication warning
+mongoose.connect(mongoUri, { useNewUrlParser: true })
+  .then(() =>  console.log('Mlab connection succesful'))
+  .catch((err) => console.error(err))
 
 app.use(morgan('dev'))
 app.use('/uploads', express.static('uploads')) //making upload folder public
@@ -34,6 +39,11 @@ app.use((req, res, next) => {
         return res.status(200).json({});
     }
     next();
+})
+
+
+app.get('/', (req, res) => {
+    res.send('MongoExpNode REST App Running from heroku! Goto /orders or /prducts or /signup.')
 })
 
 app.use('/products', productRoutes)
@@ -55,8 +65,5 @@ app.use((err, req, res, next) => {
     })
 })
 
-app.use('/', (req, res) => {
-    res.send('MEN AppRunning from heroku!. Goto /orders or /prducts or /signup.')
-})
 
 module.exports = app
