f6c196351307e2b4089976ec7db95290498799c7
Muhaimenul Islam <muhaimenulislam@iut-dhaka.edu>
2019-05-05 21:49:26 +0600

    added config

---
+++ utils/config.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+    username: process.env.MLAB_USERNAME,
+    password: process.env.MLAB_PW,
+    host: process.env.MLAB_HOST,
+    dbname: process.env.DB_NAME,
+    dbengine: process.env.DB_ENGINE
+};
